0ccd43674db03f7799c2ff36e59614dbf3cd5f94
USAMI Kenta <tadsan@zonu.me>
2018-08-10 01:06:00 +0900

    Update README

--- dist-file-mode.el
+++ dist-file-mode.el
@@ -26,6 +26,8 @@
 ;;; Commentary:
 
 ;; There are often files whose file name ends with `.dist'.
+;; (The suffix means "distributed".)
+;;
 ;; For example: `phpunit.xml.dist', `phpstan.neon.dist'
 ;;
 ;; This command removes `.dist' from file name and choises the appropriate major mode.
